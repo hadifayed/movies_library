dff2a99229fc6fdddf273e0148acd81a8f8373ac
Hadi <hagfayed@gmail.com>
2024-02-16 21:42:03 +0200

    update movie avergae rating after a new review is given

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :movie
-  
+
   validates :rating, presence: true, numericality: { only_integer: true }
+
+  after_create :update_movie_average_rating
+
+  def update_movie_average_rating
+    MovieAverageRatingUpdateService.new(self).process_record
+  end
 end
